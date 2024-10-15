118378393d09cf0f6defb0106bb2ef0e7d10f7a6
NitinChakrawarti <nitinchakrawarti04@gmail.com>
2024-10-16 00:07:23 +0530

    suppoerter done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import DashboardLayout from "./dashboard/student"
 import LoginPage from './components/loginPage'
 import NgoDashboard from './dashboard/ngo'
+import Supporter from './dashboard/supporter'
 function App() {
 
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path='/' element={<LoginPage/>}/>
       <Route path='/student/dashboard' element={<DashboardLayout/>}/>
       <Route path='/ngo/dashboard' element={<NgoDashboard/>} />
+      <Route path='/supporter/dashboard' element={<Supporter/>} />
      </Routes>
     </>
   )
